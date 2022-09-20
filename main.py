bec2d165d6d3cb92465ff904c245e066fd25dd82
palinalif <kollakleina@hotmail.com>
2022-09-20 10:05:44 +0000

    Changed the dataset, started preprocessing

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sb
 
-df = pd.read_csv('master.csv')
-print(df)
+df = pd.read_csv('smoke_detection_iot.csv')
+# Remove missing values here, before we split it into X and y?
+X = df.drop(["Unnamed: 0", "UTC", "CNT", "Fire Alarm"], axis=1)
+y = df["Fire Alarm"]
